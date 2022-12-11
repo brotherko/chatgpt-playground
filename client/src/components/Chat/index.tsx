1308c6e99146669b778bc5bb7557381cd49a4025
Marty Ko <marty.ko@kadensacapital.com>
2022-12-11 18:55:56 +0800

    update input positioning

--- client/src/components/Chat/index.tsx
+++ client/src/components/Chat/index.tsx
@@ -1,12 +1,23 @@
-import './styles.scss'
-import { MainContainer, ChatContainer, ConversationHeader, MessageList, MessageSeparator, Message, TypingIndicator, MessageInput, Button } from '@chatscope/chat-ui-kit-react';
-import {  useApi } from '../../hooks/useApi';
+import './styles.scss';
+import {
+  MainContainer,
+  ChatContainer,
+  ConversationHeader,
+  MessageList,
+  MessageSeparator,
+  Message,
+  TypingIndicator,
+  MessageInput,
+  Button,
+} from '@chatscope/chat-ui-kit-react';
+import { useApi } from '../../hooks/useApi';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowsRotate } from '@fortawesome/free-solid-svg-icons';
 
-
 export const Chat = () => {
-  const { data, post, loading, reset } = useApi(process.env.REACT_APP_API_BASE_URL as string)
+  const { data, post, loading, reset } = useApi(
+    process.env.REACT_APP_API_BASE_URL as string
+  );
 
   return (
     <div style={{ position: 'relative', height: '100vh' }}>
@@ -15,25 +26,44 @@ export const Chat = () => {
           <ConversationHeader>
             <ConversationHeader.Content userName="ChatGPT Playground" />
             <ConversationHeader.Actions>
-              <Button disabled={loading} icon={<FontAwesomeIcon icon={faArrowsRotate} />} onClick={reset} />
+              <Button
+                disabled={loading}
+                icon={<FontAwesomeIcon icon={faArrowsRotate} />}
+                onClick={reset}
+              />
             </ConversationHeader.Actions>
           </ConversationHeader>
-          <MessageList typingIndicator={loading && <TypingIndicator content='ChatGPT is typing'/>}>
-          <MessageSeparator as="h2">⚡ Chat history will be gone after refresh</MessageSeparator>
+          <MessageList
+            typingIndicator={
+              loading && <TypingIndicator content="ChatGPT is typing" />
+            }
+          >
+            <MessageSeparator as="h2">
+              ⚡ Chat history will be gone after refresh
+            </MessageSeparator>
             {data &&
               data.map((conversation: any, i: any) => (
                 <Message
                   key={i}
                   model={{
                     message: conversation.message,
-                    direction: conversation.sender === 'human' ? "outgoing" : "incoming",
+                    direction:
+                      conversation.sender === 'human' ? 'outgoing' : 'incoming',
                     position: 'single',
                   }}
                 />
               ))}
-              
           </MessageList>
-          <MessageInput placeholder="Type message here" onSend={(v) => post(v)} attachButton={false} />
+          <MessageInput
+            style={{
+              position: 'absolute',
+              bottom: 0,
+              width: '100%',
+            }}
+            placeholder="Type message here"
+            onSend={(v) => post(v)}
+            attachButton={false}
+          />
         </ChatContainer>
       </MainContainer>
     </div>
